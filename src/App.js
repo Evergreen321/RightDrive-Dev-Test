7e3a8477ee73aed5aa77b2a4dfa3b7c06ab1b5f2
Yuliia <yolochka321@gmail.com>
2023-03-07 18:03:12 -0500

    Card layout, pagination and button components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ImgMediaCard from "./ImgMediaCard/ImgMediaCard";
+import PaginationLink from "./PaginationLink/PaginationLink";
+import ContainedButton from "./ContainedButton/ContainedButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>RightDrive Dev Test</h1>
+        <ImgMediaCard />
+        <PaginationLink />
+        <ContainedButton />
       </header>
     </div>
   );
