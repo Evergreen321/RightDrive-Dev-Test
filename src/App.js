ce4977354dd1141bd96df13f95bb43870e24d3f6
Yuliia <yolochka321@gmail.com>
2023-03-07 17:05:01 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>RightDrive Dev Test</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
