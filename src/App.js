963de67a3ae7f990c134632c49481f8c118df884
Yuliia <yolochka321@gmail.com>
2023-03-08 14:59:34 -0500

    Integrate free cryptoAPI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 import ImgMediaCard from "./ImgMediaCard/ImgMediaCard";
 import PaginationLink from "./PaginationLink/PaginationLink";
 import ContainedButton from "./ContainedButton/ContainedButton";
+import ServerModal from "./ServerModal/ServerModal";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets", {
+      method: "GET",
+      redirect: "follow",
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setList(data.data);
+      })
+      .catch((error) => {
+        setError(true);
+        console.log("error", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>RightDrive Dev Test</h1>
-        <ImgMediaCard />
+        {error && <ServerModal />}
+        {list.map((item) => (
+          <ImgMediaCard {...item} />
+        ))}
         <PaginationLink />
         <ContainedButton />
       </header>
